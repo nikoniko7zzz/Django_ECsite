3c4dd9e4f694c9ce45c1a935059e500a3feba26c
matsuo <matsuokuniko7@gmail.com>
2022-01-14 00:00:27 +0900

    category_tag_lists.html add

--- config/custom_context_processors.py
+++ config/custom_context_processors.py
@@ -1,11 +1,16 @@
 from django.conf import settings
-from base.models import Item
+from base.models import Item, Category, Tag
 
-
+# 売れてるもの順（在庫が少ない順）
 def base(request):
     items = Item.objects.filter(is_published=True)
+    categorys = Category.objects.all
+    tags = Tag.objects.order_by('name')
     return {
         'TITLE': settings.TITLE,
         'ADDTIONAL_ITEMS': items,
-        'POPULAR_ITEMS': items.order_by('-sold_count')
+        'POPULAR_ITEMS': items.order_by('-sold_count'),
+        'CATEGORY_LISTS' : categorys,
+        'TAG_LISTS' : tags
     }
+
